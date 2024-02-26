6cea18538c6bd6b2f89b141f456f55fca7e8f6b9
Kowsar Islam <kowsarulislam@gmail.com>
2024-02-26 20:45:30 +0200

    Few coding added

---
+++ index.js
@@ -0,0 +1,39 @@
+// Example of basic Arrays
+
+//let vegetables = ["Caroot","Brocoli","Tomatoo","Spinach","Cucumber" ]
+//console.log(vegetables);
+//console.log(vegetables.length);// This is basically to see the length of the arrays information. Here it will be 5  as there are 5 vegetables item.
+//Also length works as a property in JS which will show the value. 
+//let marks = [50, 70, 80, 80, 40]
+//console.log(marks);
+//console.log(marks.length);
+
+//Array indices(indices is plural of index)
+
+// using array we can change anything as per our need. Example given below.
+//let marks = [50, 70, 80, 80, 40]
+//marks[1] = 20
+//console.log(marks);//Here 70 changes to 20. As per index no 70 is in 1 serial/position. So we put serial 1 inside the array under the same variable name and mention the required new no
+//let vegetables = ["Caroot","Brocoli","Tomatoo","Spinach","Cucumber" ]
+//vegetables[2] = "Potatoo"
+//console.log(vegetables);// Here Tomatoo replaced by the Potatoo
+
+//How to use loop in arrays
+
+//for loop
+
+//let cities = ["Helsinki", "Tampere", "Oulu", "Turku"]
+//for (let index = 0; index < cities.length; index++) {
+    //console.log(cities[2]);
+    
+//}
+
+//for-of loop
+
+//let cities = ["Helsinki", "Tampere", "Oulu", "Turku"]
+//for (let city of cities) {
+    //console.log(city);
+//}
+
+
+
