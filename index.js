1fbc1b8d0adaa500d45683a8fead482677e99370
Kowsar Islam <kowsarulislam@gmail.com>
2024-02-29 11:51:47 +0200

    added few coding

--- index.js
+++ index.js
@@ -35,5 +35,45 @@
     //console.log(city);
 //}
 
+//Practice : find out the average marks from a calss of a student
+
+//let marks = [85, 97, 44, 37, 76, 60];
+
+//let sum = 0;
+
+//for (let value of marks) {
+    //sum = sum + value
+//}
+
+//let avg = sum/marks.length
+//console.log("avg marks of the class = ", avg);
+
+//practice > find out the value after 10% applying
+
+//Using for off loop
+
+//let items = [250, 645, 300, 900, 50];
+//let i = 0;
+
+//for (let value of items) {
+   // let offer = value/10;
+    //items[i] = items[i] - offer;
+   // console.log(`value after offer = ${items[i]}`);
+   // i++
+//}
+
+//Using for loop
+let items = [280, 800, 3000, 1580, 170];
+
+for (let i = 0; i < items.length; i++) {
+    let offer = items[i] / 10;
+    items[i] = items[i] - offer
+    //items[i] -=  offer// this is second option write a code
+}
+console.log(items);
+
+
+
+
 
 
